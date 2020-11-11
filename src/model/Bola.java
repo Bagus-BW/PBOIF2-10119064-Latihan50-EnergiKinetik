51959d70986217ed9bdf7af95bb1b683d474cfb3
Bagus_24 <Bagus_24@Bagus24>
2020-11-11 18:54:43 +0700

    new commit

--- src/model/Bola.java
+++ src/model/Bola.java
@@ -15,15 +15,15 @@ package model;
  * dan usaha dari sebuah bola baseball yang dilempar
  */
 public class Bola {
-    private int massa;
+    private double massa;
     private int kecepatan;
     
-    public Bola(int massa, int kecepatan){
+    public Bola(double massa, int kecepatan){
         this.massa = massa;
         this.kecepatan = kecepatan;
     }
 
-    public int getMassa() {
+    public double getMassa() {
         return massa;
     }
 
@@ -31,23 +31,27 @@ public class Bola {
         return kecepatan;
     }
     
-    private double hitungEnergiKinetik(int massa, int kecepatan){
-        return 0.5 * (massa * Math.pow(kecepatan, 2));
+    private double convertGramToKg(double massa){
+        return massa/1000;
     }
     
-    private double hitungUsaha(int massa, int kecepatan){
+    private double hitungEnergiKinetik(double massa, int kecepatan){
+        return 0.5 * (convertGramToKg(massa) * Math.pow(kecepatan, 2));
+    }
+    
+    private double hitungUsaha(double massa, int kecepatan){
         double ekAwal = 0;
         double ekAkhir = hitungEnergiKinetik(massa, kecepatan);
         
         return (ekAkhir - ekAwal);
     }
     
-    public void tampil(int massa, int kecepatan){
+    public void tampil(double massa, int kecepatan){
         System.out.println("=====Program Hitung Energi Kinetik dan Usaha=====");
         System.out.println("Massa : " + massa);
         System.out.println("Kecepatan : " + kecepatan);
         System.out.println();
-        System.out.println("Energi Kinetik : " + hitungEnergiKinetik(massa, kecepatan) + " joule");
-        System.out.println("Usaha : " + hitungUsaha(massa, kecepatan) + " joule");
+        System.out.printf("Energi Kinetik : %.3f joule \n",  hitungEnergiKinetik(massa, kecepatan));
+        System.out.printf("Usaha : %.3f joule \n", hitungUsaha(massa, kecepatan));
     }
 }
